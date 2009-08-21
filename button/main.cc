165fdc39a1f5c329d226a9e086b1073bfa885329
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-08-21 23:52:55 +0000

    Placed the pushbutton in a widget to improve the layout.

--- button/main.cc
+++ button/main.cc
@@ -1,16 +1,25 @@
 #include <QApplication>
 #include <QObject>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
 #include "clickhandler.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    QWidget win;
+    win.setWindowTitle("Qt4 Button Demo");
+    
+    QVBoxLayout layout;
+
     QPushButton button("Click me!");
+    layout.addWidget(&button);
     ClickHandler handler;
 
     QObject::connect(&button, SIGNAL(clicked()), &handler, SLOT(message()));
 
-    button.show();
+    win.setLayout(&layout);
+    win.show();
     return app.exec();
 }
